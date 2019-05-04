75e1b50d8e4c3799d675ac44668ccccefcb58ad2
kwakkwakkwak <kwakkj112@naver.com>
2019-05-04 18:38:47 +0900

    todo list ver. velopert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+    id = 3
+
+    state = {
+        input : '',
+        todos: [
+            { id: 0, text: ' 리액트 소개', checked: false },
+            { id: 1, text: ' 리액트 소개', checked: true },
+            { id: 2, text: ' 리액트 소개', checked: false }
+        ]
+
+    }
+    render() {
+        const {input , todos} = this.state;
+        const {
+            handleChange,
+            handleCreate,
+            handleKeyPress,
+            handleToggle,
+            handleRemove
+        } = this;
+        return (
+            <TodoListTemplate form={
+                <Form
+                    value = {input}
+                    onKeyPress={handleKeyPress}
+                    onChange={handleChange}
+                    onCreate={handleCreate}
+                />}>
+                <TodoItemList
+                    todos={todos}
+                    onToggle={handleToggle}
+                    onRemove={handleRemove}
+                />
+            </TodoListTemplate>
+        );
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value
+        });
+    }
+
+    handleCreate = () => {
+        const { input, todos } = this.state;
+        this.setState({
+            todos: todos.concat({
+                id : this.id++,
+                text: input,
+                checked : false
+            })
+        });
+
+    }
+
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleCreate();
+        }
+    }
+
+    handleToggle = (id) => {
+
+        const { todos } = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+        const selected = todos[index];
+
+        this.setState({
+            todos: [
+                ...todos.slice(0, index),
+                {
+                    ...selected,
+                    checked: !selected.checked
+                },
+                ...todos.slice(index + 1, todos.length)
+            ]
+        });
+
+
+    }
+
+    handleRemove = (id) => {
+        const { todos } = this.state;
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        });
+    }
 }
 
 export default App;
